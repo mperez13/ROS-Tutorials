ab47e5bc60fcb7c0a204bde984a6ffcd5a461a50
mfperez13 <mfperez13@gmail.com>
2016-11-18 23:29:41 -0800

    erased sensorstrea

---
+++ navigation_ws/build/robotSetup_SensorStreams/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marifer/ROS-Tutorials/navigation_ws/src/robotSetup_SensorStreams
+# Build directory: /home/marifer/ROS-Tutorials/navigation_ws/build/robotSetup_SensorStreams
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
