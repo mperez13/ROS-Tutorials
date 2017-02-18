4d6ae8980e4e8fa407d042545333a3f6fccafdd8
mfperez13 <mfperez13@gmail.com>
2017-02-17 20:36:53 -0800

    update control_ros_ws

---
+++ controlros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/marifer/ROS-Tutorials/controlros_ws/src
+# Build directory: /home/marifer/ROS-Tutorials/controlros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mybot_description)
+subdirs(mybot_gazebo)
